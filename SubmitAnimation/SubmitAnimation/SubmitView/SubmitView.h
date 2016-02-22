ed3649e075faab08baee460de6b618ad3c2cce77
DMDavid <894566327@qq.com>
2016-02-22 13:06:18 +0800

    init

---
+++ SubmitAnimation/SubmitAnimation/SubmitView/SubmitView.h
@@ -0,0 +1,14 @@
+//
+//  SubmitView.h
+//  SubmitAnimation
+//
+//  Created by David on 16/2/19.
+//  Copyright © 2016年 com.david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SubmitView : UIView
+
+@end
